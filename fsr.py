3e350a0c7de5483f77934927b5511df5487c2243
Anton Goncharenko <agonch@hotmail.com>
2021-03-11 00:53:53 -0800

    comments

--- fsr.py
+++ fsr.py
@@ -3,29 +3,26 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-#define the pin that goes to the circuit
 pin_to_circuit = 6
 
 def rc_time (pin_to_circuit):
     count = 0
 
-    #Output on the pin for
+    # Drain Capacitor
     GPIO.setup(pin_to_circuit, GPIO.OUT)
     GPIO.output(pin_to_circuit, GPIO.LOW)
     time.sleep(0.2)
 
-    #Change the pin back to input
+    # Change the pin back to input
     GPIO.setup(pin_to_circuit, GPIO.IN)
 
-    #Count until the pin goes high
+    # Count until the pin goes high
     while (GPIO.input(pin_to_circuit) == GPIO.LOW):
         count += 1
 
     return count / 1000
 
-#Catch when script is interrupted, cleanup correctly
 try:
-    # Main loop
     while True:
         print(rc_time(pin_to_circuit))
 except KeyboardInterrupt:
--- pixelated_stream_gpio.py
+++ pixelated_stream_gpio.py
@@ -56,15 +56,15 @@ def prepCamera(camera):
 def rc_time (pin_to_circuit):
     count = 1
 
-    #Output on the pin for
+    # Drain the capacitor
     GPIO.setup(pin_to_circuit, GPIO.OUT)
     GPIO.output(pin_to_circuit, GPIO.LOW)
     time.sleep(0.1)
 
-    #Change the pin back to input
+    # Change the pin back to input
     GPIO.setup(pin_to_circuit, GPIO.IN)
 
-    #Count until the pin goes high
+    # Count until the pin goes high
     while (GPIO.input(pin_to_circuit) == GPIO.LOW):
         count += 1
 
