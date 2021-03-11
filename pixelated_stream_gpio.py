b74ae12b8ae444ec014b2f74eae3658a8ce0284c
Anton Goncharenko <agonch@hotmail.com>
2021-03-10 20:58:45 -0800

    fsr

--- pixelated_stream_gpio.py
+++ pixelated_stream_gpio.py
@@ -20,7 +20,7 @@ Streamed continously into an RGB Array:
 np.set_printoptions(threshold=sys.maxsize)
 RESOLUTION = (640, 480)
 PIXELATED_RESOLUTION = (64, 64)
-SHOULD_FLIP_RGB = False
+RGB_MULTIPLIER = 1
 
 def flipRGB(rgb_array):
     return rgb_array[:, :, ::-1]
@@ -31,8 +31,7 @@ Grab the input size of the PiRGBArray, and resize the input to
 "pixelated" size. Return output image.
 """
 def pixelateFrame(rgb_array):
-    if SHOULD_FLIP_RGB:
-        rgb_array = flipRGB(rgb_array)
+    rgb_array = rgb_array * RGB_MULTIPLIER
     height, width = rgb_array.shape[:2]
     temp = cv2.resize(rgb_array, PIXELATED_RESOLUTION, interpolation=cv2.INTER_LINEAR)
     output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
@@ -52,13 +51,30 @@ def prepCamera(camera):
     return camera
 
 
+def rc_time (pin_to_circuit):
+    count = 0
+
+    #Output on the pin for
+    GPIO.setup(pin_to_circuit, GPIO.OUT)
+    GPIO.output(pin_to_circuit, GPIO.LOW)
+    time.sleep(0.2)
+
+    #Change the pin back to input
+    GPIO.setup(pin_to_circuit, GPIO.IN)
+
+    #Count until the pin goes high
+    while (GPIO.input(pin_to_circuit) == GPIO.LOW):
+        count += 1
+
+    return count / 10000
+
 def gpio_callback(channel):
-    global SHOULD_FLIP_RGB
-    SHOULD_FLIP_RGB = GPIO.input(channel) == GPIO.HIGH
+    global RGB_MULTIPLIER
+    RGB_MULTIPLIER = rc_time(6)
     if GPIO.input(channel) == GPIO.HIGH:
-        print('\n▼  at ' + str(datetime.datetime.now()))
+        print('▼  at ' + str(datetime.datetime.now()))
     else:
-        print('\n ▲ at ' + str(datetime.datetime.now()))
+        print(' ▲ at ' + str(datetime.datetime.now()))
 
 
 def launch_camera():
@@ -79,7 +95,7 @@ def launch_camera():
 
 try:
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(6, GPIO.IN) # pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(6, GPIO.BOTH, callback=gpio_callback)
     launch_camera()
 finally:
